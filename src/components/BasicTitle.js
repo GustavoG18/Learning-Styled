816888fb13e2c03472b0de6f8f9718c7cc0dd8ca
Gustavo Guerrero <gguerrerof98@gmail.com>
2021-12-30 17:52:59 -0500

    feature: management props in styled-components

--- src/components/BasicTitle.js
+++ src/components/BasicTitle.js
@@ -3,4 +3,6 @@ import styled from "styled-components";
 export const BasicTitle = styled.h1`
   text-align: center;
   text-transform: capitalize;
-`;
+  /* color: ${({ special }) => special && 'red'}; This sentence if i need a prop to set a specific value */
+  color: ${({ special }) => special ? 'red':'blue'};
+`; 
